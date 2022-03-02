3d53d3c5dd4170bf3ffe05a7321fa3a64d233db7
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-03-02 14:02:50 +0200

    Fixed project tructure

---
+++ Paws4Me/Paws4Me/Controllers/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  AdoptPet.swift
+//  Paws4Me
+//
+//  Created by Kirsty-lee Walker 20/02/2022
+//
+
+import UIKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var petTable: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var filteredPetObject: AdoptPet?
+    var adoptPetObject: AdoptPet?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getAdoption()
+        petTable.delegate = self
+        petTable.dataSource = self
+        searchBar.delegate = self
+    }
+
+    func getAdoption() {
+        
+        guard let url = URL(string: Constants.adoptURL) else { fatalError("Missing URL") }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.makeRequest(url: request as URLRequest, model: AdoptPet.self) { [weak self] result in
+            switch result {
+            case .success(let petData):
+                self?.adoptPetObject = petData
+                self?.filteredPetObject = petData
+                self?.petTable.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+           if let pageCount = filteredPetObject?.page?.count {
+               return pageCount
+           } else {
+             return 0
+           }
+   }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: "petAdoptCell") as? AnimalTableViewCell else {
+           return UITableViewCell()
+        }
+        let adoptablepet = filteredPetObject
+        cell.index = indexPath.row
+        cell.pet = adoptablepet
+        cell.setNeedsLayout()
+       return cell
+   }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "showPetSingleDetails", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let destination = segue.destination as? PetSingleDetailsViewController {
+            guard let rowIndex = petTable.indexPathForSelectedRow?.row else {return}
+            guard let pageItem = adoptPetObject?.page?[rowIndex] else {return}
+            
+            if let pagePetName = pageItem.name {
+            destination.nameOfPet = pagePetName
+            }
+            if let pagePetAge = pageItem.age {
+            destination.imagOfPet = pagePetAge
+            }
+            if let pageGender = pageItem.sex {
+            destination.genderOfPet = pageGender
+            }
+            if let pagePetImage = pageItem.animalImage {
+            destination.imagOfPet = pagePetImage
+            }
+            if let pagePetBreed = pageItem.animalSpeciesBreed?.petBreedName {
+            destination.breedOfPet = pagePetBreed
+            }
+        }
+            }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filteredPetObject = nil
+       
+        if searchText == "" {
+            filteredPetObject = adoptPetObject
+        } else {
+
+            guard let petObject = adoptPetObject else {
+                return
+            }
+                if  searchText.lowercased().contains("Female") {
+                filteredPetObject = petObject
+        }
+        }
+        
+        self.petTable.reloadData()
+    }
+}
+extension UIViewController {
+    func displayAlert(title: String, message: String, buttonTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: buttonTitle, style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
---
+++ Paws4Me/Paws4Me/UserInterfaces/PetSingleDetailsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AdoptPet.swift
+//  Paws4Me
+//
+//  Created by Kirsty-lee Walker 20/02/2022
+//
+
+import UIKit
+
+class PetSingleDetailsViewController: UIViewController {
+
+    @IBOutlet weak var petImageView: UIImageView!
+    @IBOutlet weak var petName: UILabel!
+    @IBOutlet weak var petAge: UILabel!
+    @IBOutlet weak var petGender: UILabel!
+    @IBOutlet weak var petBreedName: UILabel!
+    var singlePet: AdoptPet?
+    var nameOfPet = ""
+    var breedOfPet = ""
+    var genderOfPet = ""
+    var ageOfPet = ""
+    var imagOfPet = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        petName.text=nameOfPet
+        petAge.text=ageOfPet
+        petBreedName.text=breedOfPet
+
+        petGender.text=genderOfPet
+        guard let url = URL(string: imagOfPet) else { return }
+
+        UIImage.loadFrom(url: url) { [self] image in
+            self.petImageView.layer.cornerRadius = 10
+            self.petImageView.image = image
+            self.petImageView.backgroundColor = UIColor(red: 55/255.0, green: 21/255.0, blue: 67/255.0, alpha: 1)
+            view.addSubview(petImageView)
+        }
+    }
+}
+
+extension UIImage {
+    public static func loadFrom(url: URL, completion: @escaping (_ image: UIImage?) -> Void) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    completion(UIImage(data: data))
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
+    }
+}
