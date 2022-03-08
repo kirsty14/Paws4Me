289859713d632e45061b5e5bdc8d79c65967cea6
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-03-08 12:30:43 +0200

    Added in a local json object in case my api goes down again

--- Paws4Me/Paws4Me/Utilities/URLSession+Extension.swift
+++ Paws4Me/Paws4Me/Utilities/URLSession+Extension.swift
@@ -42,3 +42,16 @@ extension URLSession {
         apiTask.resume()
     }
 }
+
+private func readLocalFile(forName name: String) -> Data? {
+    do {
+        if let bundlePath = Bundle.main.path(forResource: name,
+                                             ofType: "json"),
+            let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+            return jsonData
+        }
+    } catch {
+        print(error)
+    }
+    return nil
+}
