b9d5fafdbcbafb69993fbb572f9b2b133e028e0e
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-03-15 16:16:51 +0200

    Made the signin controller look nicer

--- Paws4Me/Paws4Me/Extensions/UITextfieldExtension.swift
+++ Paws4Me/Paws4Me/Extensions/UITextfieldExtension.swift
@@ -11,8 +11,10 @@ import UIKit
 extension UITextField {
 func addUnderline() {
     let border = CALayer()
+    let width = CGFloat(1.0)
+    border.borderColor = UIColor.myAppPurple.cgColor
     border.frame = CGRect(x: 0, y: self.frame.height * 0.8, width: self.frame.width, height: 2)
-    border.backgroundColor = UIColor.myAppPurple.cgColor
+    border.borderWidth = width
     self.borderStyle = .none
     self.layer.addSublayer(border)
 
@@ -20,10 +22,10 @@ func addUnderline() {
 
 func addErrorBorderBoth() {
     let border = CALayer()
-         let width = CGFloat(1.0)
-         border.borderColor =  UIColor.myAppError.cgColor
-    border.frame = CGRect(x: 0, y: self.frame.height * 0.8, width: self.frame.width, height: 2)
-         border.borderWidth = width
+    let width = CGFloat(1.0)
+    border.borderColor =  UIColor.myAppError.cgColor
+    border.frame = CGRect(x: 0, y: self.frame.height * 0.95, width: self.frame.width, height: 2)
+    border.borderWidth = width
     self.borderStyle = .none
     self.layer.addSublayer(border)
     self.layer.masksToBounds = true
