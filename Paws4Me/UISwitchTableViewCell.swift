39f790eb56fd7149ed04dfd3728a798f57f41ce2
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-04-01 11:22:29 +0200

    I have added in the first page of the dynamic adoption form

---
+++ Paws4Me/UISwitchTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  UISwitchTableViewCell.swift
+//  Paws4Me
+//
+//  Created by Kirsty-Lee Walker on 2022/04/01.
+//
+
+import UIKit
+
+class UISwitchTableViewCell: UITableViewCell {
+
+    static let identifier = "UISwitchCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
