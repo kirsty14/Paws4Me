94ccfc1e3445d5138683224964e1cab037a84bf5
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-04-09 17:32:29 +0200

    I have got 100% coverage for my login unit test

--- Paws4Me/Paws4MeTests/loginTest.swift
+++ Paws4Me/Paws4MeTests/loginTest.swift
@@ -15,40 +15,35 @@ class LoginTest: XCTestCase {
 
     override func setUp() {
         mockViewModelDelegate = MockViewModelDelegate()
-        self.signInViewModel = SignInViewModel(delegate: MockViewModelDelegate())
+        self.signInViewModel = SignInViewModel(delegate: mockViewModelDelegate)
     }
 
     func testFailingLoginEmptyFail() {
         signInViewModel.loginUser(username: "", password: "")
-        mockViewModelDelegate.showError(errorMessage: "Please fill in your username and password")
         XCTAssertFalse(mockViewModelDelegate.isSuccesRouting)
         XCTAssertTrue(mockViewModelDelegate.isError)
     }
 
     func testFailingWrongUsernameFail() {
         signInViewModel.loginUser(username: "a", password: "TestPass123")
-        mockViewModelDelegate.showError(errorMessage: "Login Error")
         XCTAssertFalse(mockViewModelDelegate.isSuccesRouting)
         XCTAssertTrue(mockViewModelDelegate.isError)
     }
 
     func testFailingWrongPasswrdFail() {
         signInViewModel.loginUser(username: "Admin", password: "t")
-        mockViewModelDelegate.showError(errorMessage: "Login Error")
         XCTAssertFalse(mockViewModelDelegate.isSuccesRouting)
         XCTAssertTrue(mockViewModelDelegate.isError)
     }
 
     func testLoginWrongUsernamePasswordFail() {
         signInViewModel.loginUser(username: "a", password: "t")
-        mockViewModelDelegate.showError(errorMessage: "Login Error")
         XCTAssertFalse(mockViewModelDelegate.isSuccesRouting)
         XCTAssertTrue(mockViewModelDelegate.isError)
     }
 
     func testLoginCorrectUsernamePasswordPass() {
         signInViewModel.loginUser(username: "Admin", password: "TestPass123")
-        mockViewModelDelegate.successRouting()
         XCTAssert(mockViewModelDelegate.isSuccesRouting)
         XCTAssertFalse(mockViewModelDelegate.isError)
     }
