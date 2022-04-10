061964d842a361e7d6b3aa011a2799ecd26bb8e4
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-04-10 08:56:50 +0200

    Busy with the single et details unit tests

--- Paws4Me/Paws4MeTests/SinglePetDetailsTest.swift
+++ Paws4Me/Paws4MeTests/SinglePetDetailsTest.swift
@@ -13,18 +13,48 @@ class SinglePetDetailsTest: XCTestCase {
     private var singlePetViewmodel: SinglePetViewModel!
     private var mockSinglePetRepository: MockSinglePetRepository!
 
+    override func setUp() {
+        mockSinglePetRepository = MockSinglePetRepository()
+        singlePetViewmodel = SinglePetViewModel(repository: mockSinglePetRepository)
+  }
+
+    func testSavedPetByName_ReturnsTrue() {
+        mockSinglePetRepository.shouldPass = true
+        var isSaved = mockSinglePetRepository.isPetSaved(petName: "Baba")
+        mockSinglePetRepository.setMockData()
+        XCTAssertEqual(singlePetViewmodel.singlePetName, "Baba")
+    }
+
 }
 
-class MockSinglePetRepository {
-    func isPetSaved(petName: String) -> Bool {
-        return true
+class MockSinglePetRepository: SinglePetRepository {
+    var shouldPass = false
+    var singlePetViewmodel = SinglePetViewModel(repository: MockSinglePetRepository())
+    var isPetSaved = false
+
+    func setMockData() {
+        var singlePetData: AdoptPet
+        if shouldPass {
+        singlePetData =  AdoptPet(count: 1, page: [Page(age: "Adult", center: Center(city: "JHB", id: 1,
+                                      lat: 12.89, lon: 13.67, name: "Center JHB", services: "none",
+                                      state: "SA", zipcode: "2456"), color: "Ginger", desc: "Pretty cat",
+                                      petId: 1, name: "Baba", animalImage: "link", sex: "Female",
+                                      animalSpeciesBreed: SpeciesBreed(petBreedName: "Tabby", speciesID: 1,
+                                                         petSpecies: "Cat", petYouthName: "Kitten"))])
+        singlePetViewmodel.setSinglePetObject(petObject: singlePetData)
+        singlePetViewmodel.setSelectedPetIndex(indexPet: 0)
+        } else {
+        }
     }
 
-    private var setMockData: AdoptPet {
-        return AdoptPet(count: 1, page: [Page(age: "Adult", center: Center(city: "JHB", id: 1, lat: 12.89, lon: 13.67,
-                        name: "Center JHB", services: "none", state: "SA", zipcode: "2456"),
-                        color: "Ginger", desc: "Pretty cat", petId: 1, name: "Baba", animalImage: "link", sex: "Female",
-                        animalSpeciesBreed: SpeciesBreed(petBreedName: "Tabby", speciesID: 1,
-                                                         petSpecies: "Cat", petYouthName: "Kitten"))])
+    override func isPetSaved(petName: String) -> Bool {
+        if shouldPass {
+            isPetSaved = true
+        } else {
+            isPetSaved = false
+        }
+
+        return isPetSaved
     }
+
 }
