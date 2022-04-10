4ade4c11562d0352b48502082991fc2b29fb0025
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-04-10 07:32:54 +0200

    Starting with unit testing for single pet details logic

---
+++ Paws4Me/Paws4MeTests/SinglePetDetailsTest.swift
@@ -0,0 +1,19 @@
+//
+//  Paws4MeTests.swift
+//  Paws4MeTests
+//
+//  Created by Kirsty-Lee Walker on 2022/02/23.
+//
+
+import XCTest
+@testable import Paws4Me
+
+class SinglePetDetailsTest: XCTestCase {
+
+    private var singlePetViewmodel: SinglePetViewModel!
+    private var mockSinglePetRepository: MockSinglePetRepository!
+
+}
+
+class MockSinglePetRepository {
+}
