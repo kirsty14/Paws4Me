b81f3f63170ad0bd9ae9b9cdf7b56b6cf92c8f65
Kirsty-Lee Walker <kwalker@DVTL102832.local>
2022-04-12 11:27:44 +0200

    I have added in a collection view for my home screen

---
+++ Paws4Me/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  Paws4Me
+//
+//  Created by Kirsty-Lee Walker on 2022/04/12.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var homeImage: UIImageView!
+    @IBOutlet weak var homeLabel: UILabel!
+}
---
+++ Paws4Me/HomeCollectionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeCollectionViewController.swift
+//  Paws4Me
+//
+//  Created by Kirsty-Lee Walker on 2022/04/12.
+//
+
+import Foundation
+import UIKit
+
+class HomeCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let homeImages: [String] = ["dogCV", "catCV", "dog3CV"]
+    let homeTitle: [String] = ["All pets", "Favourites", "Adoption Progress"]
+    let viewControllerIdentifiers: [String] = ["AllPetDetailViewController", "LocalPetViewController",
+                                               "applicationForm"]
+    let homeViewColors = [UIColor(named: "AllPetColor"), UIColor(named: "favouritesPetColor"),
+                          UIColor(named: "adoptPetColor")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return homeImages.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let destination = storyboard?.instantiateViewController(withIdentifier:
+                                                                viewControllerIdentifiers[indexPath.row])
+        self.navigationController?.pushViewController(destination!, animated: true)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
+                                                            for: indexPath) as? HomeCollectionViewCell
+        else { return UICollectionViewCell() }
+
+        cell.homeImage.image = UIImage(named: homeImages[indexPath.row])
+        cell.homeLabel.text = homeTitle[indexPath.row]
+        cell.cellView.backgroundColor = homeViewColors[indexPath.row]
+        cell.cellView.layer.cornerRadius = 30
+        return cell
+    }
+}
